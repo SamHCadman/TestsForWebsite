2833ad2659ff515522e6c75f7c7a1a9e6ca04d67
Flo <florentin.molimard@gmail.com>
2016-04-18 22:49:12 +0200

    Changing color title working

--- js/index.js
+++ js/index.js
@@ -4,22 +4,37 @@
 
 const DELAY = 100;
 const ELEMENT = 'idxTtl';
+const COEF_RGB = [1, 2, 1];
 var rgb = [255, 192, 203];
+var sensRgb = [1, 1, 1];
 
 function changeColor(){
-    console.log("changeColor");
-    if (rgb[2]<255){
-        rgb[2]++;
+    console.log("changeColor::STARTING\n\t" + sensRgb.toString());
+    if (rgb[2] == 255 || rgb[2] == 0){
+        sensRgb[2] = -1 * sensRgb[2];
+        rgb[2] += sensRgb[2] * COEF_RGB[2];
+    }else if (rgb[2] < 255 && rgb[2] > 0){
+        rgb[2] += sensRgb[2]  * COEF_RGB[2];
     } else {
-        if (rgb[1]<255) {
-            rgb[1]++;
-        } else {
-            if (rgb[0]<255){
-                rgb[0]++;
-            }else {
-                rgb = [0, 0, 0];
-            }
-        }
+        rgb[2] = 203;
+    }
+
+    if (rgb[1] >= 254 || rgb[1] <= 1){
+        sensRgb[1] = -1 * sensRgb[1];
+        rgb[1] += (sensRgb[1] * COEF_RGB[1]);
+    }else if (rgb[1] < 254 && rgb[1] > 1){
+        rgb[1] += (sensRgb[1] * COEF_RGB[1]);
+    } else {
+        rgb[1] = 192;
+    }
+
+    if (rgb[0] == 255 || rgb[0] == 0){
+        sensRgb[0] = -1 * sensRgb[0];
+        rgb[0] += sensRgb[0] * COEF_RGB[0];
+    }else if (rgb[0] < 255 && rgb[0] > 0){
+        rgb[0] += sensRgb[0] * COEF_RGB[0];
+    } else {
+        rgb[0] = 255;
     }
     document.getElementById(ELEMENT).style.color = "rgb(" + rgb[0] + ", " + rgb[1] + ", " + rgb[2] + ")";
     // document.getElementById(ELEMENT).style.color = rgb;
